0f6cebcbe696ae0d474d43c81784ae7552962676
chrisais9 <chrisais9@naver.com>
2022-01-11 14:07:29 +0900

    :sparkles: 마이페이지 화면 추가

---
+++ SiSo/View/MyPage/MyPageView.swift
@@ -0,0 +1,78 @@
+//
+//  MyPageView.swift
+//  SiSo
+//
+//  Created by 구형모 on 2022/01/11.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    
+    @State var isGlobalNotificationEnabled = true
+    @State var isMarketingNotificationEnabled = true
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            
+            Form {
+                Section {
+                    HStack {
+                        Spacer()
+                        VStack(spacing: 10) {
+                            Image("profile_dummy")
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 50, height: 50)
+                            HStack(spacing: 5) {
+                                Text("구형모")
+                                    .font(NotoSans.regular(size: 15))
+                                Image(systemName: "chevron.right")
+                                    .imageScale(.small)
+                            }
+                            Text("시소와 함께한 지 501일째")
+                                .font(NotoSans.regular(size: 12))
+                                .foregroundColor(.gray)
+                        }
+                        .padding()
+                        Spacer()
+                    }
+                }
+                
+                Section {
+                    Toggle("앱 알림", isOn: $isGlobalNotificationEnabled)
+                    Toggle("마케팅 수신 동의", isOn: $isMarketingNotificationEnabled)
+                } header: {
+                    Text("알림 설정")
+                }
+                
+                Section {
+                    NavigationLink("개인정보 처리 방침") {
+                        
+                    }
+                    NavigationLink("이용 약관") {
+                        
+                    }
+                    Button {
+                        
+                    } label: {
+                        Text("로그아웃")
+                            .foregroundColor(.black)
+                    }
+
+                }
+
+            }
+            Spacer()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MyPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            MyPageView()
+        }
+    }
+}
