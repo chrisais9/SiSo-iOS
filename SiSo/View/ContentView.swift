841302bd7ef6d90191eb8fe2c5853e393fcfee42
chrisais9 <chrisais9@naver.com>
2022-01-23 17:37:19 +0900

    :bug: 뷰들이 비정상으로 작동할 수 있었던 버그 수정

--- SiSo/View/ContentView.swift
+++ SiSo/View/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 import RealmSwift
 
+enum SelectedTab: Hashable {
+    case home
+    case mypage
+}
 struct ContentView: View {
     
     @ObservedResults(User.self) var users
+    
+    @State var selectedTab: SelectedTab = SelectedTab.home
+    
+    @State var isInstructionDialogActive: Bool = false
+    @State var isLoginPageActive: Bool = false
     
     init() {
         if users.isEmpty {
@@ -23,23 +32,69 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            TabView {
+            TabView(selection: $selectedTab) {
                 HomeView()
-                    .navigationBarHidden(true)
                     .tabItem {
                         Image(systemName: "house")
                         Text("홈")
                     }
+                    .tag(SelectedTab.home)
                 
                 if !users.isEmpty{
                     MyPageView(user: users.first!)
-                        .navigationBarHidden(true)
                         .tabItem {
                             Image(systemName: "person.circle")
                             Text("마이페이지")
                         }
+                        .tag(SelectedTab.mypage)
                 }
             }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    if selectedTab == SelectedTab.home {
+                        Button {
+                            isInstructionDialogActive.toggle()
+                        } label: {
+                            Image(systemName: "info.circle")
+                                .resizable()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        Button {
+                            isLoginPageActive.toggle()
+                        } label: {
+                            Image(systemName: "bell")
+                                .resizable()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                    }
+                }
+            }
+            .simpleDialog(isShowing: $isInstructionDialogActive, dialogContent: {
+                VStack {
+                    Text("그래, 그거 설명서")
+                        .font(NotoSans.bold(size: 18))
+                        .padding(.vertical, 17)
+                    
+                    Text("1. 장소 선택하러 가기 클릭\n2. 투표를 함께할 친구 초대\n3. 필터 선택\n4. 내 동네 맛집 리스트 확인\n5. 친구들과 투표시작\n6. 1위 결과 발표!")
+                        .font(NotoSans.regular(size: 13))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 13)
+                    
+                    Divider()
+                    Button {
+                        isInstructionDialogActive.toggle()
+                    } label: {
+                        Text("확인")
+                            .font(NotoSans.medium(size: 16))
+                            .foregroundColor(.black)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding(.bottom)
+                    }
+                }
+            })
         }
         .onChange(of: users, perform: { newValue in
             if users.isEmpty {
--- SiSo/View/Home/HomeView.swift
+++ SiSo/View/Home/HomeView.swift
@@ -8,34 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State var isLoginPageActive: Bool = false
-    @State var isInstructionDialogActive: Bool = false
+    
     @State var isSelectFriendViewActive: Bool = false
     
     var body: some View {
+        ScrollView(showsIndicators: false) {
         VStack {
             VStack {
-                HStack {
-                    Spacer()
-                    Button {
-                        isInstructionDialogActive.toggle()
-                    } label: {
-                        Image(systemName: "info.circle")
-                            .resizable()
-                            .foregroundColor(.black)
-                            .frame(width: 20, height: 20)
-                    }
-                    
-                    Button {
-                        isLoginPageActive.toggle()
-                    } label: {
-                        Image(systemName: "bell")
-                            .resizable()
-                            .foregroundColor(.black)
-                            .frame(width: 20, height: 20)
-                    }
-                }
-                
                 VStack(alignment: .leading, spacing: 17) {
                     Text("오늘 어디가지?")
                         .font(NotoSans.bold(size: 30))
@@ -48,7 +27,8 @@ struct HomeView: View {
                 
                 Text("이미지 or 관련 일러스트")
                     .font(NotoSans.light(size: 17))
-                    .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 200)
+                    .frame(height: 200)
+                    .frame(minWidth: 0, maxWidth: .infinity)
                     .overlay(
                         RoundedRectangle(cornerRadius: 3)
                             .stroke(Color.black, lineWidth: 1)
@@ -85,34 +65,15 @@ struct HomeView: View {
             }
             Spacer()
         }
-        .simpleDialog(isShowing: $isInstructionDialogActive, dialogContent: {
-            VStack {
-                Text("그래, 그거 설명서")
-                    .font(NotoSans.bold(size: 18))
-                    .padding(.vertical, 17)
-                
-                Text("1. 장소 선택하러 가기 클릭\n2. 투표를 함께할 친구 초대\n3. 필터 선택\n4. 내 동네 맛집 리스트 확인\n5. 친구들과 투표시작\n6. 1위 결과 발표!")
-                    .font(NotoSans.regular(size: 13))
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 13)
-                
-                Divider()
-                Button {
-                    isInstructionDialogActive.toggle()
-                } label: {
-                    Text("확인")
-                        .font(NotoSans.medium(size: 16))
-                        .foregroundColor(.black)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding(.bottom)
-                }
-            }
-        })
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        NavigationView {
+            HomeView()
+                .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
